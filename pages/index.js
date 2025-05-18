2a5b1f53ea6899f4ab9261d580dd8d60d748d9bc
Kaitlynn Oxley <97kaity@gmail.com>
2025-05-17 18:29:12 -0700

    Stage7

--- pages/index.js
+++ pages/index.js
@@ -65,8 +65,9 @@ const section = new Section({
 
 const addTodoPopup = new PopupWithForm({
   popupSelector: "#add-todo-popup",
-  handleFormSubmit: (evt) => {
-    PopupWithForm._getInputValues();
+  handleFormSubmit: (inputValues) => {
+    inputValues = { name: inputValues.name, dateInput: inputValues.date };
+    todosList.append(inputValues);
     addTodoPopup.close();
   },
 });
